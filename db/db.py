bcc19f8e5fea2a8137d7255f2f89a8e1f64932ad
dari4006 <darion.wong660@hotmail.com>
2023-05-16 13:03:14 +0930

    database fix

--- db/db.py
+++ db/db.py
@@ -1,7 +1,7 @@
 import os
 import psycopg2
 import psycopg2.extras
-DB_URL = "dbname=videogame_db"
+DB_URL = "dbname=videogamedb_458t"
 
 def sql(query, parameters=[]):
   connection = psycopg2.connect(DB_URL) # open connection
